e70eb8c583f5e7caba456ef5935d4ef4d57d17ae
White.Moon <117890040+Colombina-White@users.noreply.github.com>
2025-10-17 23:06:22 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -51,5 +51,4 @@ document.querySelectorAll(".project-card .yt-button").forEach(btn => {
 
 // 關閉彈窗
 closeBtn.onclick = () => popup.style.display = "none";
-
 window.onclick = e => { if (e.target === popup) popup.style.display = "none"; };
